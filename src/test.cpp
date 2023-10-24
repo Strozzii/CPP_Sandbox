12683d4360f61726009c089cc8c19f0e85a6cdf2
Stroz_Dennis <stroz.dennis@gmail.com>
2023-10-24 10:01:36 +0200

    Init

---
+++ src/test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Dennis Stroz on 24.10.23.
+//
+
+#include "test.h"
---
+++ src/test.h
@@ -0,0 +1,8 @@
+//
+// Created by Dennis Stroz on 24.10.23.
+//
+
+#ifndef OPC_TEST_H
+#define OPC_TEST_H
+
+#endif //OPC_TEST_H
